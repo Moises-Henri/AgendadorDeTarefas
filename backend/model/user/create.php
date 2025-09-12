19331a60c57118b06418eef8cd7c682af772918f
MoisesHenri <moisessouza4699@gmail.com>
2025-09-11 21:51:28 -0300

    11/09 = progresso

---
+++ backend/model/user/create.php
@@ -0,0 +1,29 @@
+<?php
+
+include("../connection/conn.php");
+
+if (empty($_REQUEST['NAME']) || empty($_REQUEST['EMAIL'])  || empty($_REQUEST['PASSWORD'])  || 
+  empty($_REQUEST['LEVEL'])) {
+    $dados = array(
+        "type" == "error",
+        "message" == "Existe(m) campo(s) obrigatório(s) não preenchido(s)"
+
+    );
+} else {
+    try{
+    $sql = "INSERT INTO USER (NAME, EMAIL, PASSWORD, LEVEL) VALUES (?, ?, ?, ?)"
+    $stmt = $conn->prepare($sql);
+    $stmt->execute([
+        $_REQUEST['NAME'],
+        $_REQUEST['EMAIL'],
+        $_REQUEST['PASSWORD'],
+        $_REQUEST['LEVEL']
+    ]);
+} catch{
+
+    
+}
+}
+
+
+?>
